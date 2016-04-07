022904b9a23cf88c9a8b26151d6dabc50a3e5ae3
Efia-lin <lyhfinalworld@gmail.com>
2016-04-07 11:01:28 +0800

    注册界面

---
+++ ByWay/app/src/main/java/com/example/efia/byway/ByWay.java
@@ -0,0 +1,26 @@
+package com.example.efia.byway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+
+public class ByWay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_by_way);
+
+        WebView myWebView = (WebView) findViewById(R.id.webView);
+        WebSettings wSet = myWebView.getSettings();
+        wSet.setJavaScriptEnabled(true);
+
+        myWebView.loadUrl("file:///android_asset/hellopage.html");
+
+        
+    }
+}
