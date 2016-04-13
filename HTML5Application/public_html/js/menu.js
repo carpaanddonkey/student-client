183c47749a06c621cdf4436245489d5a56510ff6
unknown <>
2016-04-13 22:51:52 +0800

    2016/04/13

---
+++ HTML5Application/public_html/js/menu.js
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function changeimg1(obj){
+    document.getElementById("cart_bar").src="Images/icon/cart.png";
+    document.getElementById("me_bar").src="Images/icon/me.png";
+    obj.src="Images/icon/shop1.png";
+}
+                
+
+function changeimg2(obj){
+    document.getElementById("menu_bar").src="Images/icon/shop.png";
+    document.getElementById("me_bar").src="Images/icon/me.png";
+    obj.src="Images/icon/cart1.png";
+}
+                
+function changeimg3(obj){
+    document.getElementById("cart_bar").src="Images/icon/cart.png";
+    document.getElementById("menu_bar").src="Images/icon/shop.png";
+    obj.src="Images/icon/me1.png";
+}
