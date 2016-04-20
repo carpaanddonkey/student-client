dfc5c7505bc5648b2af4aab5d07a45a0efcea996
unknown <>
2016-04-20 23:40:54 +0800

    no message

--- HTML5Application/public_html/js/menu.js
+++ HTML5Application/public_html/js/menu.js
@@ -4,22 +4,11 @@
  * and open the template in the editor.
  */
 
+function qrcode_show(){
+    document.getElementById('qrcode').style.display='block';//show的display属性设置为block（显示）
+}
+      
+function qrcode_none(){
+    document.getElementById('qrcode').style.display='none';//show的display属性设置为none（隐藏）
+}
 
-function changeimg1(obj){
-    document.getElementById("cart_bar").src="Images/icon/cart.png";
-    document.getElementById("me_bar").src="Images/icon/me.png";
-    obj.src="Images/icon/shop1.png";
-}
-                
-
-function changeimg2(obj){
-    document.getElementById("menu_bar").src="Images/icon/shop.png";
-    document.getElementById("me_bar").src="Images/icon/me.png";
-    obj.src="Images/icon/cart1.png";
-}
-                
-function changeimg3(obj){
-    document.getElementById("cart_bar").src="Images/icon/cart.png";
-    document.getElementById("menu_bar").src="Images/icon/shop.png";
-    obj.src="Images/icon/me1.png";
-}
