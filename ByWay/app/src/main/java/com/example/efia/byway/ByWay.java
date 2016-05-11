bedb3fc5c41421f3b675e18ec7a7e9a0dd2fb085
Efia-lin <lyhfinalworld@gmail.com>
2016-05-11 23:14:12 +0800

    实现部分数据加载
    禁止跳出系统浏览器

--- ByWay/app/src/main/java/com/example/efia/byway/ByWay.java
+++ ByWay/app/src/main/java/com/example/efia/byway/ByWay.java
@@ -1,24 +1,35 @@
 package com.example.efia.byway;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
-public class ByWay extends AppCompatActivity {
+public class ByWay extends Activity  {
 
+    private WebView myWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_by_way);
+        setContentView(R.layout.activity_by_way);
 
-        WebView myWebView = (WebView) findViewById(R.id.webView);
+        myWebView = (WebView) findViewById(R.id.webView);
+        myWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view,String url)
+            {
+                view.loadUrl(url);
+                return true;
+            }
+        });
         WebSettings wSet = myWebView.getSettings();
-        wSet.setJavaScriptEnabled(true);
+        wSet.setJavaScriptEnabled(true);//设置在web中调用js
 
-        myWebView.loadUrl("file:///android_asset/hellopage.html");
+        //myWebView.loadUrl("file:///android_asset/hellopage.html");
+        myWebView.loadUrl("http://web.byway.net.cn/student/hellopage.html");
 
-        
     }
+
+
 }
---
+++ HTML5Application/public_html/js/gbin1.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('#webcam').photobooth().on("image",function( event, dataUrl ){	
+		$('.nopic').hide();
+		$( "#pictures" ).append( '<img src="' + dataUrl + '" >');
+	});
+	
+	if(!$('#webcam').data('photobooth').isSupported){
+		alert('HTML5 webcam is not supported by your browser, please use latest firefox, opera or chrome!');
+	}	
+	
+});
